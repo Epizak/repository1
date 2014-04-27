9b605b882981f2b77ea8f7bb2fb227d45348dfcb
gitlab3 <epizsak@gmail.com>
2014-04-26 19:15:01 -0500

    commit

---
+++ colores.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+main () 
+{ 
+system ("color f2"); 
+printf ("color ( f color fondo --- 2 color letra)\n\n "); 
+printf ("puedes ir jugando con esas combinaciones "); 
+system ("pause"); 
+} 
